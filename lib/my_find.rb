5c0d6130ec74265c30befe0e46b55619d5f90647
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-27 04:59:22 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,10 +3,11 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
+  if
   yield(collection[i])
-  return collection[i] if
-  i = i + 1
+  return collection[i] 
   end
+  i = i + 1 
 end
 
 collection = (1..100).to_a
